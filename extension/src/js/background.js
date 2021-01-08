3dcddcaf431564a8c5de49eeb5e74f15bb46906c
Julius S <jluuiss@gmail.com>
2021-01-09 03:15:04 +0800

    build

--- extension/src/js/background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(tab => {
-    // for the current tab, inject the "inject.js" file & execute it
-    chrome.tabs.executeScript(tab.ib, {
-        file: 'inject.bundle.js',
-    })
-})
